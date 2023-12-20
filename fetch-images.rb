16353967713a71cacfbc5c694f006f3c23578807
fgrelated <152218577+fgrelated@users.noreply.github.com>
2023-12-20 19:54:27 +0100

    Make fetcher robust (burns, opt whitelist)
    Burns are made to prevent hitting the same URL multiple times.
    
    Optional whitelist is to prevent fetching images from 3P domains.

--- fetch-images.rb
+++ fetch-images.rb
@@ -1,22 +1,49 @@
 #!/usr/bin/env ruby
 
+# Outdir for the images
 IMAGES = File.join(File.dirname($0), 'images')
-AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-BLACKLIST_HOST = %w[c.statcounter.com static.pjmedia.com www.moonofalabama.org]
-BLACKLIST_URL = %w[
-  https://timedotcom.files.wordpress.com/2017/02/steve-bannon-cover-time.jpg
-  https://theconservativetreehouse.files.wordpress.com/2018/11/jeff-sessions-resignation.jpg
-  https://theconservativetreehouse.files.wordpress.com/2018/09/trump-tweet-declassification-reversal.jpg
-  https://theconservativetreehouse.files.wordpress.com/2018/09/trump-sessions-3.jpg
-]
-SLEEP_BETWEEN_REQUESTS = 1
+
+# User agent to use
+AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+
+# Hosts that won't be fetched from
+BLACKLIST_HOST = %w[]
+
+# URLs that won't be fetched (sha1 or direct)
+BLACKLIST_URL = %w[]
+
+# Do not consider any 3rd party domains
+NO_3P_DOMAINS = ENV['NO_3P_DOMAINS'] || false
+
+# Host whitelist (list of hosts to restrict to, when non-empty. empty = all)
+WHITELIST_HOST = NO_3P_DOMAINS ?  %w[
+  a5ebcfcbf2c40f0b281230d90d016e002a3a31a0 #f9fw
+  a6bbd2a3643af52dc61d8153a61a9a0def0bb5ab #gtfw
+  c0d8a5ddc695543da739000c21b9835517f3c7da #fgs
+  b9cc3619507297511e54cd4260dc98a8e3952ea2 #fb
+  cee7b1b87daed8be93d5bfd99630384d00743475 #f9w
+  13232157c18d9235962e61eb0bc9ecff47cd5555 #gtw
+] : []
+
+# How long to sleep between requests
+SLEEP_BETWEEN_REQUESTS = 0.2
+
+# Should we dryrun (show, don't fetch)
 DRYRUN = ENV['DRYRUN'] ? true : false
+
+# Where to store SHA1-hashed URLs that failed (so we avoid them next time)
+BURN_DIR = File.join(File.dirname($0), 'burns')
+
+# How many times does the URL have to fail before it's considered burned
+MAX_BURNS = 1
 
 require 'pp'
 require 'cgi'
 require 'set'
 require 'fileutils'
 require 'mimemagic' # gem install mimemagic
+require 'digest/sha1'
+require_relative 'static_burns'
 
 # ----------------------------------------------------------------------------
 # Common structures
@@ -30,12 +57,24 @@ Comment = Struct.new(:id, :author, :date, :content, :parent_id, :user_id)
 # ----------------------------------------------------------------------------
 
 def extract_images(content)
-  out = []
+  out = Set.new
   content.scan(/<img.*?>/i).each do |c|
     if c =~ /src=(['"])(.*?)\1/
       i = CGI.unescapeHTML($2)
       i.sub!(/\?.*/, '') if i =~ /wordpress.com\/.*\?([wh]=\d+&?)+$/
-      out << i
+      out.add(i)
+    end
+  end
+
+  if content.index('<')
+    # at least some html content
+    content.gsub(/<.*?>/, '').scan(/(https?:\/\/[^\s]*\.(png|gif|jpe?g))/i) do |i, _|
+      out.add(i)
+    end
+  else
+    # no html content
+    content.gsub(/<.*?>/, '').scan(/(\s|\A)(https?:\/\/[^\s]*\.(png|gif|jpe?g))(\s|\z)/i) do |_, i, _|
+      out.add(i)
     end
   end
   out
@@ -60,34 +99,74 @@ def image_file(url)
   File.join(IMAGES, url.sub(/^https?:\/\//, ''))
 end
 
+def burn(url)
+  FileUtils.mkdir_p(BURN_DIR)
+  File.open(File.join(BURN_DIR, Digest::SHA1.hexdigest(url)), 'a') { |f| f.write('.') }
+rescue Object
+  nil
+end
+
+def burned?(url)
+  hd = Digest::SHA1.hexdigest(url)
+  return true if STATIC_BURNS.include?(hd)
+  n = File.join(BURN_DIR, hd)
+  FileTest.size(n) >= MAX_BURNS
+rescue Object
+  nil
+end
+
 def maybe_save_image(jar, url)
   fn = image_file(url)
+  h = Digest::SHA1.hexdigest(url)
+
   if FileTest.file?(fn)
     return :cached # already exists
   end
 
   # url blacklist
-  return :blacklisted if BLACKLIST_URL.include?(url)
+  return :blacklisted if BLACKLIST_URL.include?(url) || BLACKLIST_URL.include?(h)
   # host blacklist
   if url =~ /\/\/(.*?)\//
-    return :blacklisted if BLACKLIST_HOST.include?($1)
+    host = $1
+    hh = Digest::SHA1.hexdigest(host)
+    return :blacklisted if BLACKLIST_HOST.include?(host) || BLACKLIST_HOST.include?(hh)
   end
+
+  # host whitelist. when not empty, fetches are restricted to these host(s)
+  unless WHITELIST_HOST.empty?
+    if url =~ /\/\/(.*?)\//
+      host = $1
+      hh = Digest::SHA1.hexdigest(host)
+      return :blacklisted unless WHITELIST_HOST.include?(host) || WHITELIST_HOST.include?(hh)
+    end
+  end
+
+  return :burned if burned?(url)
 
   return :dryrun if DRYRUN
 
   res = fetch_url(jar, url)
   if res.nil?
+    burn(url)
     return "can't fetch image" # errorneous http code
   end
 
-  type = MimeMagic.by_magic(res).type
+  type = MimeMagic.by_magic(res)
+  type = type.nil? ? "unknown" : type.type
   unless type =~ /^image\//
+    burn(url)
     return "invalid file type: #{type}"
   end
 
-  FileUtils.mkdir_p(File.dirname(fn))
-  File.open(fn + ".tmp", 'w') { |f| f.write(res) }
-  File.rename(fn + ".tmp", fn)
+  begin
+    FileUtils.mkdir_p(File.dirname(fn))
+    File.open(fn + ".tmp", 'w') { |f| f.write(res) }
+    File.rename(fn + ".tmp", fn)
+  rescue Object
+    STDERR.puts "BUG: can't store #{url.inspect}: #$!"
+    burn(url)
+    return "can't store image"
+  end
   :fetched
 end
 
@@ -136,9 +215,12 @@ How to get it:
 
   STDOUT.sync = true
   sz = images.size
-  images.each_with_index do |image, idx|
+  images.sort_by { rand }.each_with_index do |image, idx|
     print "#{idx+1}/#{sz} ... \r"
     case res = maybe_save_image(jar, image)
+    when :burned
+      stats[res] += 1
+      STDERR.puts "- #{image} :: burned"
     when :blacklisted
       stats[res] += 1
       STDERR.puts "- #{image} :: blacklisted"
